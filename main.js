e90e0e649094e47399b9dc5ea0011fd399928115
Mateus Silva <mateusbs427@gmail.com>
2022-04-21 18:56:41 -0300

    :pencil: menu transition and header scroll

--- main.js
+++ main.js
@@ -15,3 +15,17 @@ for(const link of links) {
     nav.classList.remove('show')
   })
 }
+
+// adicionar sombra ao header quando der scroll na página
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function() {
+  if (window.scrollY >= navHeight) {
+    // scroll é maior que a altura do header
+    header.classList.add('scroll')
+  } else {
+    // menor que a altura do header
+    header.classList.remove('scroll')
+  }
+})
